ca5549f9ddac79ebfdd21c5958b48f383a6ed324
Roxym3 <2493889475@qq.com>
2026-04-29 16:04:20 +0800

    add: revoke logic for chaincode and listener

--- chaincode-go/cross-domain-auth.go
+++ chaincode-go/cross-domain-auth.go
@@ -22,10 +22,54 @@ type AuthTicket struct {
 	IssuerCert   string `json:"issuer_cert"`
 }
 
-func (s *SmartContract) IssueTicket(ctx contractapi.TransactionContextInterface, ticketID string, targetDomain string, owner string, expiryTime int64) error {
-	issuerMSPID, err := ctx.GetClientIdentity().GetMSPID()
+func (s *SmartContract) getClientMSPID(ctx contractapi.TransactionContextInterface) (string, error) {
+	mspID, err := ctx.GetClientIdentity().GetMSPID()
 	if err != nil {
-		return fmt.Errorf("failed to get issuerMSP info: %v", err)
+		return "", fmt.Errorf("MSPID: %w", err)
+	}
+	return mspID, nil
+}
+
+func (s *SmartContract) getTicket(ctx contractapi.TransactionContextInterface, ticketID string) (*AuthTicket, error) {
+	ticketJSON, err := ctx.GetStub().GetState(ticketID)
+	if err != nil {
+		return nil, fmt.Errorf("ticketID: %w", err)
+	}
+
+	if ticketJSON == nil {
+		return nil, fmt.Errorf("tikcet not exists: %s", ticketID)
+	}
+
+	var ticket AuthTicket
+	if err := json.Unmarshal(ticketJSON, ticket); err != nil {
+		return nil, fmt.Errorf("Unmarshal json: %w", err)
+	}
+
+	return &ticket, nil
+}
+
+func (s *SmartContract) putAndEvent(ctx contractapi.TransactionContextInterface, ticket *AuthTicket, eventName string) error {
+	ticketJSON, err := json.Marshal(ticket)
+	if err != nil {
+		return fmt.Errorf("Marshal json: %w", err)
+	}
+
+	if err := ctx.GetStub().PutState(ticket.TicketID, ticketJSON); err != nil {
+		return fmt.Errorf("Put state: %w", err)
+	}
+
+	if eventName != "" {
+		if err := ctx.GetStub().SetEvent(eventName, ticketJSON); err != nil {
+			return fmt.Errorf("Set Event: %w", err)
+		}
+	}
+	return nil
+}
+
+func (s *SmartContract) IssueTicket(ctx contractapi.TransactionContextInterface, ticketID string, targetDomain string, owner string, expiryTime int64) error {
+	issuerMSPID, err := s.getClientMSPID(ctx)
+	if err != nil {
+		return err
 	}
 
 	if issuerMSPID != "BusinessMSP" {
@@ -57,36 +101,13 @@ func (s *SmartContract) IssueTicket(ctx contractapi.TransactionContextInterface,
 		IssuerCert:   certPEM,
 	}
 
-	ticketJSON, err := json.Marshal(ticket)
-	if err != nil {
-		return err
-	}
-
-	err = ctx.GetStub().PutState(ticketID, ticketJSON)
-	if err != nil {
-		return fmt.Errorf("failed to put state:%v", err)
-	}
-
-	err = ctx.GetStub().SetEvent("TicketIssuedEvent", ticketJSON)
-	if err != nil {
-		return fmt.Errorf("failed to set event:%v", err)
-	}
+	s.putAndEvent(ctx, &ticket, "TicketIssuedEvent")
 
 	return nil
 }
 
 func (s *SmartContract) VerifyTicket(ctx contractapi.TransactionContextInterface, ticketID string) (bool, error) {
-	ticketJSON, err := ctx.GetStub().GetState(ticketID)
-	if err != nil {
-		return false, fmt.Errorf("failed to read ticket:%v", err)
-	}
-
-	if ticketJSON == nil {
-		return false, fmt.Errorf("ticket doesn't exist:%v", ticketID)
-	}
-
-	var ticket AuthTicket
-	err = json.Unmarshal(ticketJSON, &ticket)
+	ticket, err := s.getTicket(ctx, ticketID)
 	if err != nil {
 		return false, err
 	}
@@ -96,8 +117,7 @@ func (s *SmartContract) VerifyTicket(ctx contractapi.TransactionContextInterface
 		return false, fmt.Errorf("failed to get time stamp:%v", err)
 	}
 
-	currentTime := txTimeStamp.Seconds
-	if currentTime > ticket.ExpiryTime {
+	if txTimeStamp.Seconds > ticket.ExpiryTime {
 		return false, fmt.Errorf("failed to verify.Ticket has been expired at %d", ticket.ExpiryTime)
 	}
 
@@ -105,15 +125,42 @@ func (s *SmartContract) VerifyTicket(ctx contractapi.TransactionContextInterface
 		return false, fmt.Errorf("ticket has been revoked")
 	}
 
-	verifierMSPID, err := ctx.GetClientIdentity().GetMSPID()
+	verifierMSPID, err := s.getClientMSPID(ctx)
 	if err != nil {
-		return false, fmt.Errorf("failed to get verifier MSP ID:%v", err)
+		return false, err
 	}
 
 	if verifierMSPID != ticket.TargetDomain {
 		return false, fmt.Errorf("Permission denied")
 	}
 	return true, nil
+}
+
+func (s *SmartContract) RevokeTicket(ctx contractapi.TransactionContextInterface, ticketID string) error {
+	callerMSPID, err := s.getClientMSPID(ctx)
+	if err != nil {
+		return err
+	}
+
+	if callerMSPID != "BusinessMSP" {
+		return fmt.Errorf("Permission denied, current identity: %s", callerMSPID)
+	}
+
+	ticket, err := s.getTicket(ctx, ticketID)
+	if err != nil {
+		return err
+	}
+
+	if ticket.TargetDomain != callerMSPID {
+		return fmt.Errorf("Permission denied,only issuer can revoke this ticket")
+	}
+
+	if ticket.IsRevoked == true {
+		return fmt.Errorf("Ticket has been revoked")
+	}
+
+	ticket.IsRevoked = true
+	return s.putAndEvent(ctx, ticket, "TicketRevokedEvent")
 }
 
 func (s *SmartContract) TicketExists(ctx contractapi.TransactionContextInterface, ticketID string) (bool, error) {
