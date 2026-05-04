45d27c1530b831cb8a12a0127050325d84eb196b
Roxym3 <2493889475@qq.com>
2026-05-04 19:21:09 +0800

    decoupling

--- app/listener/main.go
+++ app/listener/main.go
@@ -10,9 +10,13 @@ import (
 	"listener/pkg/listener"
 	"listener/pkg/pool"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 const MaxRetries = 16
@@ -32,38 +36,70 @@ func run() error {
 		return fmt.Errorf("load config: %w", err)
 	}
 
-	mongoClient, err := database.InitMongoDB(&cfg.Database)
+	mongoClient, ticketColl, err := setupDB(cfg)
 	if err != nil {
-		return fmt.Errorf("database error: %w", err)
+		return err
 	}
 	defer database.Disconnect(mongoClient)
+
+	taskPool := pool.NewPool(100)
+
+	eventListener, err := startFabric(ctx, ticketColl, taskPool, cfg)
+	if err != nil {
+		return err
+	}
+
+	apiServer := startServer(cfg, ticketColl)
+	<-ctx.Done()
+	shutdown(apiServer, taskPool, eventListener)
+	log.Printf("exited")
+	return nil
+}
+
+func setupDB(cfg *config.Config) (*mongo.Client, *mongo.Collection, error) {
+	mongoClient, err := database.InitMongoDB(&cfg.Database)
+	if err != nil {
+		return nil, nil, fmt.Errorf("database error: %w", err)
+	}
 
 	ticketColl := mongoClient.Database(cfg.Database.DBName).Collection(cfg.Database.Collection)
 	log.Println("successfully connect to mongodb")
 
-	taskPool := pool.NewPool(100)
+	return mongoClient, ticketColl, nil
+}
+
+func startFabric(ctx context.Context, ticketColl *mongo.Collection, taskPool *pool.TaskPool, cfg *config.Config) (*listener.FabricListener, error) {
 	eventListener, err := listener.NewFabricListener(ticketColl, taskPool, &cfg.Fabric)
 	if err != nil {
-		return fmt.Errorf("create fabric listener: %w", err)
+		return nil, fmt.Errorf("create fabric listener: %w", err)
 	}
 
 	m := fabric.NewManger(cfg.Fabric.Peers, &cfg.Fabric, eventListener)
 	go m.Start(ctx)
+	return eventListener, nil
+}
 
-	// Start API Server
-	apiServer := api.NewServer(ticketColl)
+func startServer(cfg *config.Config, ticketColl *mongo.Collection) *api.Server {
+	apiServer := api.NewServer(ticketColl, cfg.Fabric.MspID)
 	go func() {
-		if err := apiServer.Start("8081"); err != nil {
+		if err := apiServer.Start("8081"); err != nil && err != http.ErrServerClosed {
 			log.Printf("API Server error: %v", err)
 		}
 	}()
 
-	<-ctx.Done()
+	return apiServer
+}
+
+func shutdown(apiServer *api.Server, taskPool *pool.TaskPool, eventListener *listener.FabricListener) error {
 	log.Println("cleaning up")
 
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	if err := apiServer.Shutdown(shutdownCtx); err != nil {
+		return err
+	}
 	taskPool.Wait()
 	eventListener.Wm.End()
-
-	log.Printf("exited")
 	return nil
 }
--- app/listener/pkg/api/server.go
+++ app/listener/pkg/api/server.go
@@ -3,6 +3,7 @@ package api
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 	"time"
@@ -11,91 +12,129 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type Server struct {
-	collection *mongo.Collection
-}
+var (
+	ErrTikcetNotFound = errors.New("ticket not found")
+	ErrTikcetRevoked  = errors.New("ticket revoked")
+	ErrTikcetExpired  = errors.New("ticket expired")
+	ErrDomainMismatch = errors.New("domain mismatch")
+)
 
-func NewServer(col *mongo.Collection) *Server {
-	return &Server{collection: col}
+type (
+	Server struct {
+		collection *mongo.Collection
+		mspID      string
+		httpSever  *http.Server
+	}
+	Ticket struct {
+		TicketID     string `bson:"ticket_id"`
+		TargetDomain string `bson:"target_domain"`
+		IsRevoked    bool   `bson:"is_revoked"`
+		ExpiryTime   int64  `bson:"expiry_time"`
+	}
+)
+
+func NewServer(col *mongo.Collection, mspID string) *Server {
+	return &Server{
+		collection: col,
+		mspID:      mspID,
+	}
 }
 
 func (s *Server) Start(port string) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/verify", s.handleVerify)
+	s.httpSever = &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
 
 	log.Printf("Starting API server on port %s", port)
-	return http.ListenAndServe(":"+port, mux)
+	return s.httpSever.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	if s.httpSever != nil {
+		log.Printf("Shutting down http server")
+		return s.httpSever.Shutdown(ctx)
+	}
+	return nil
 }
 
 func (s *Server) handleVerify(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		writeJSON(w, http.StatusMethodNotAllowed, false, "method not allowed")
 		return
 	}
 
 	ticketID := r.URL.Query().Get("ticketId")
 	if ticketID == "" {
-		http.Error(w, "Missing ticketId", http.StatusBadRequest)
+		writeJSON(w, http.StatusBadRequest, false, "missing parameter")
 		return
 	}
 
 	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 	defer cancel()
 
-	var ticket struct {
-		TicketID     string `bson:"ticket_id"`
-		TargetDomain string `bson:"target_domain"`
-		IsRevoked    bool   `bson:"is_revoked"`
-		ExpiryTime   int64  `bson:"expiry_time"`
-	}
-
-	err := s.collection.FindOne(ctx, bson.M{"_id": ticketID}).Decode(&ticket)
+	err := s.validateTicket(ctx, ticketID)
 	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			w.WriteHeader(http.StatusUnauthorized)
-			json.NewEncoder(w).Encode(map[string]interface{}{
-				"success": false,
-				"message": "凭证不存在",
-			})
-			return
-		}
-		log.Printf("Database error: %v", err)
-		http.Error(w, "Database error", http.StatusInternalServerError)
+		handleErr(w, err)
 		return
 	}
 
-	currentTime := time.Now().Unix()
+	writeJSON(w, http.StatusOK, true, "Verified")
+}
+
+func writeJSON(w http.ResponseWriter, statusCode int, success bool, message string) {
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(map[string]any{
+		"success": success,
+		"message": message,
+	})
+}
+
+func handleErr(w http.ResponseWriter, err error) {
+	switch err {
+	case ErrTikcetNotFound, ErrTikcetRevoked, ErrTikcetExpired, ErrDomainMismatch:
+		writeJSON(w, http.StatusUnauthorized, false, err.Error())
+	default:
+		log.Printf("Internal Sever error: %vr", err)
+		writeJSON(w, http.StatusInternalServerError, false, "internal server error")
+	}
+}
+
+func (s *Server) validateTicket(ctx context.Context, ticketID string) error {
+	ticket, err := s.getTicket(ctx, ticketID)
+	now := time.Now().Unix()
+	if err != nil {
+		return err
+	}
 
 	if ticket.IsRevoked {
-		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"success": false,
-			"message": "凭证已被撤销",
-		})
-		return
+		return ErrTikcetRevoked
 	}
 
-	if ticket.ExpiryTime < currentTime {
-		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"success": false,
-			"message": "凭证已过期",
-		})
-		return
+	if ticket.TargetDomain != s.mspID {
+		return ErrDomainMismatch
 	}
 
-	if ticket.TargetDomain != "PrivacyMSP" {
-		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"success": false,
-			"message": "凭证目标域不匹配",
-		})
-		return
+	if ticket.ExpiryTime < now {
+		return ErrTikcetExpired
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]interface{}{
-		"success": true,
-		"message": "验证成功，允许登录",
-	})
+	return nil
+}
+
+func (s *Server) getTicket(ctx context.Context, ticketID string) (*Ticket, error) {
+	var ticket Ticket
+	err := s.collection.FindOne(ctx, bson.M{"_id": ticketID}).Decode(&ticket)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, ErrTikcetNotFound
+		}
+		return nil, err
+	}
+
+	return &ticket, err
 }
